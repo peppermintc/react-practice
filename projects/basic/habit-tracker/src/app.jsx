3b60f44c3e4e984c2dc1e4da436dcc3994bf1cbb
peppermintc <haminjp@naver.com>
2020-10-25 05:19:25 +0900

    habit jsx created

--- projects/basic/habit-tracker/src/app.jsx
+++ projects/basic/habit-tracker/src/app.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import './app.css';
+import Habit from './components/habit';
 
 function App() {
-  const name = 'hamin';
-  return (
-    <>
-      <h1>hi</h1>
-      {name && <h1>{name}!</h1>}
-      {['😀','😁'].map(item => <h1>{item}</h1>)}
-    </>
-  );
+  return <Habit />;
 }
 
 export default App;
