ac5313278248fc1e65834d3053440c7487338536
peppermintc <haminjp@naver.com>
2020-10-25 18:18:47 +0900

    state and props

--- projects/basic/habit-tracker/src/components/habit.jsx
+++ projects/basic/habit-tracker/src/components/habit.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
+    state = {
+        count: 0,
+    };
+
+    handleIncrement = () => {
+        this.setState({ count: this.state.count + 1 });
+    };
+
+    handleDecrement = () => {
+        const count = this.state.count;
+        this.setState({ count: count - 1 < 0 ? 0 : count - 1 });
+    };
+
     render() {
+        const { name, count } = this.props.habit;
         return (
             <li className="habit">
-                <span className="habit-name">Reading</span>
-                <span className="habit-count">8</span>
-                <button className="habit-button habit-increase">
+                <span className="habit-name">{name}</span>
+                <span className="habit-count">{count}</span>
+                <button className="habit-button habit-increase" onClick={this.handleIncrement}>
                     <i className="fas fa-plus-square"></i>
                 </button>
-                <button className="habit-button habit-decrease">
+                <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
                     <i className="fas fa-minus-square"></i>
                 </button>
                 <button className="habit-button habit-delete">
