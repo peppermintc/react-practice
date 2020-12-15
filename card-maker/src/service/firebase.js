a8ad8917c09b6fef2d7044522c626321aad1e732
Hamin <haminjp@naver.com>
2020-12-16 08:32:38 +0900

    firebase set up

---
+++ card-maker/src/service/firebase.js
@@ -0,0 +1,7 @@
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTO_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
