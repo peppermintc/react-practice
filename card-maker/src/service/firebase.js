518dda7a985b891661566f31956800960f47d46a
Hamin <haminjp@naver.com>
2020-12-16 11:48:36 +0900

    feat: google github auth login

--- card-maker/src/service/firebase.js
+++ card-maker/src/service/firebase.js
@@ -1,7 +1,10 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTO_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
